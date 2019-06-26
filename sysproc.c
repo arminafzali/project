3334434cb8c54aca4ca99f120540743fa451b574
arminafzali <arminafzali23@gmail.com>
2019-06-26 07:39:39 +0430

    Update sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -106,10 +106,6 @@ sys_getppid(void)
 int sys_wait_and_performance(void)
 {
   int *wtime, *rtime;
-
-  // get our arguments from user space
-  // return -1 in case anything goes wrong
-
   if (argptr(0, (void*)&wtime, sizeof(wtime)) < 0)
     return -1;
   if (argptr(1, (void*)&rtime, sizeof(rtime)) < 0)
